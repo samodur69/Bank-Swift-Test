7a0ed5dc2c031a3976c4e0971ef7307ae939a18b
samodur69 <69460645+samodur69@users.noreply.github.com>
2020-11-09 12:44:23 +0300

    Initial Commit

---
+++ Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank
+//
+//  Created by Alexey Nadenenko on 09.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
